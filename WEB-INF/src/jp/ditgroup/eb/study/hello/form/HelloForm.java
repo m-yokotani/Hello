c8af06465b16c5288efdff33cf07a41e5e9b8282
美遥 <@bunbun2>
2015-09-26 21:32:00 +0900

    ・計算結果などの出力もgetter/setterでの設定に変更 … （HelloFormに追加） ・エラー文を出力できるよう作成 　⇒※日本語が文字化けしてしまう問題

--- WEB-INF/src/jp/ditgroup/eb/study/hello/form/HelloForm.java
+++ WEB-INF/src/jp/ditgroup/eb/study/hello/form/HelloForm.java
@@ -17,42 +17,45 @@ public class HelloForm extends ActionForm {
 
 	private static final long serialVersionUID = 1L;
 	private String calculateMethod;
-	private String num1;
-	private String num2;
-	private String error;
+	private String strNum1;
+	private String strNum2;
+
 	private double result;
+	private String judge;
 
 	// getterメソッド
 	public String getCalculateMethod() {
 		return this.calculateMethod;
 	}
-	public String getNum1() {
-		return this.num1;
+	public String getStrNum1() {
+		return this.strNum1;
 	}
-	public String getNum2() {
-		return this.num2;
+	public String getStrNum2() {
+		return this.strNum2;
 	}
-	public String getError() {
-		return this.error;
-	}
+
 	public double getResult() {
 		return this.result;
+	}
+	public String getJudge() {
+		return this.judge;
 	}
 
 	// setterメソッド
 	public void setCalculateMethod(String calculateMethod) {
 		this.calculateMethod = calculateMethod;
 	}
-	public void setNum1(String num1) {
-		this.num1 = num1;
+	public void setStrNum1(String strNum1) {
+		this.strNum1 = strNum1;
 	}
-	public void setNum2(String num2) {
-		this.num2 = num2;
+	public void setStrNum2(String strNum2) {
+		this.strNum2 = strNum2;
 	}
-	public void setError(String error) {
-		this.error = error;
-	}
+
 	public void setResult(double result) {
 		this.result = result;
+	}
+	public void setJudge(String judge) {
+		this.judge = judge;
 	}
 }
